f60783cdea3e2be3a123204a3630314cc09ee239
Gustavo Máximo <gfmaximo97@gmail.com>
2020-06-18 18:47:09 -0300

    Add static CSS.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const homeRoutes = require('./routes/home');
 const usersRoute = require('./routes/users');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(homeRoutes);
 app.use(usersRoute);
 
